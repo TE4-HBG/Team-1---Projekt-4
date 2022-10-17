8fac9884812b221d3cd7ebbed202ea0e85ac8f16
Hugo s <hugo.sjoholm@elev.ga.ntig.se>
2022-10-17 14:36:33 +0200

    Rat controler now works
    Arrow keys and shift for movment. Press space to ""use"" powerup

---
+++ Projekt4/Assets/RatMovment.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class RatMovment : MonoBehaviour
+{
+
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 3.5f;
+    private float jumpHeight = 1.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            playerSpeed = 5.5f;
+        }
+        else
+        {
+            playerSpeed = 2.5f;
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            UsePowerUp();
+        }
+
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+
+
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+    void UsePowerUp()
+    {
+        Debug.Log("Used PowerUp lol");
+    }
+}
