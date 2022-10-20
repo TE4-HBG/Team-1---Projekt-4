b3b91c6360d8b6f0aedbd4384057f077cd4a6c70
Hugo s <hugo.sjoholm@elev.ga.ntig.se>
2022-10-20 13:06:21 +0200

    Moved usings
    Co-Authored-By: KLohger <39872370+Klohger@users.noreply.github.com>

--- Projekt4/Assets/Scripts/GameManager.cs
+++ Projekt4/Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 // may god have mercy on my soul
 using System.Linq;
+
 
 public class GameManager : MonoBehaviour
 {
