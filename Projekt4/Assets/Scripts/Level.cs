a19238fdbe34e43c46f29e67e410b91dd058be2b
Hugo s <hugo.sjoholm@elev.ga.ntig.se>
2022-10-20 13:48:49 +0200

    Made rat smaller and slowe

--- Projekt4/Assets/Scripts/Level.cs
+++ Projekt4/Assets/Scripts/Level.cs
@@ -7,16 +7,12 @@ public class Level : MonoBehaviour
 {
     public int number = 0;
     public Vector3 entranceOffset;
-    public Vector3 cameraOffset;
 
     [ExecuteInEditMode]
     public void OnDrawGizmos()
     {
         Gizmos.color = Color.magenta;
         Gizmos.DrawWireSphere(transform.position + entranceOffset, 1);
-
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawWireSphere(transform.position + cameraOffset, 1);
 
         Gizmos.color = Color.white;
     }
