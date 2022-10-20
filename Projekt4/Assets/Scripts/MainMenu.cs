0f0dd6deb7d2c5619779b7849f5b8fc248338f7c
terris03 <avramidisterris@gmail.com>
2022-10-20 13:27:29 +0200

    UI settings updated

--- Projekt4/Assets/Scripts/MainMenu.cs
+++ Projekt4/Assets/Scripts/MainMenu.cs
@@ -5,15 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject settings;
+    public void StartGame()
+    {
+        GameManager.StartGame();
+        this.gameObject.SetActive(false);
+    }
+    public void SettingsButton()
+    {
+        settings.SetActive(true);
+    }
     public void ExitButton()
     {
         Application.Quit();
         Debug.Log("Game Closed");
     }
 
-    public void StartGame()
-    {
-        GameManager.StartGame();
-        this.gameObject.SetActive(false);
-    }
+    
 }
