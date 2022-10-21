d85a2234a458b3f114160754257e942e19857de3
KLohger <fanimdude32@gmail.com>
2022-10-21 13:34:53 +0200

    Removed unnecessary usings in TileSystem

--- Projekt4/Assets/Scripts/TileSystem.cs
+++ Projekt4/Assets/Scripts/TileSystem.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.UIElements;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class TileSystem : MonoBehaviour
